35a15dfe95ac25a1f42ada84d05254e81019a46c
Nandan-V-Patel <nandanvinaypatel@gmail.com>
2025-08-08 07:45:37 +0530

    Add files via upload

--- NPTEL codes/PGM assignment1.py
+++ NPTEL codes/PGM assignment1.py
@@ -1,3 +1,11 @@
+# Q1.A positive integer m is a prime product if it can be written as p×q, where p and q are both primes. .
+
+# Write a Python function primeproduct(m) that takes an integer m as input and returns True if m is a prime product and False otherwise. (If m is not positive, your function should return False.)
+
+# Q2.Write a function delchar(s,c) that takes as input strings s and c, where c has length 1 (i.e., a single character), and returns the string obtained by deleting all occurrences of c in s. If c has length other than 1, the function should return s
+
+# Q3.Write a function shuffle(l1,l2) that takes as input two lists, 11 and l2, and returns a list consisting of the first element in l1, then the first element in l2, then the second element in l1, then the second element in l2, and so on. If the two lists are not of equal length, the remaining elements of the longer list are appended at the end of the shuffled output.
+
 def ifPrime(n):
     fac=[]
     for i in range(1,n+1):
@@ -45,4 +53,3 @@ def shuffle(l1,l2):
         if l2[i:i+1]:
             l3.append(l2[i])
     return l3
-print(shuffle([0],[1,3,5]))
